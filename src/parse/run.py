dfb5216d0322363ddf75b90297b3b755bb58a1d9
Dan Mihaila <danmihaila@gmail.com>
2014-11-10 16:58:11 +0200

    add arguments

--- src/parse/run.py
+++ src/parse/run.py
@@ -1,8 +1,29 @@
 import get_files as get_files
 import parse_files as parse_files
+import sys
+import getopt
 
 # run download
-get_files.run_download()
+# get_files.run_download()
 
 # run parse
 # parse_files.run_parse()
+
+
+def main(argv):
+    action = ''
+    try:
+        action = str(argv[0])
+    except Exception:
+        print "Note. options are download or parse"
+        sys.exit(2)
+    if action == 'download':
+        get_files.run_download()
+    elif action == 'parse':
+        parse_files.run_parse()
+    else:
+        print "Nothing to do. options are download or parse"
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
