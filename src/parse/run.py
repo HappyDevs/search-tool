2b689626ba3c61a0c2d112e89dae3e666c23b70b
Dan Mihaila <danmihaila@gmail.com>
2014-11-10 16:33:09 +0200

    get all files

--- src/parse/run.py
+++ src/parse/run.py
@@ -2,7 +2,7 @@ import get_files as get_files
 import parse_files as parse_files
 
 # run download
-# get_files.run_download()
+get_files.run_download()
 
 # run parse
-parse_files.run_parse()
+# parse_files.run_parse()
