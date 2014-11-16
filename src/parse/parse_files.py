0ccebe49c164b7a008ca96081cb5931ff8c29768
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-11-16 15:15:35 +0200

    Catching problem with badly formatted XML files

--- src/parse/parse_files.py
+++ src/parse/parse_files.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import xml.etree.ElementTree as ET
 import json
+from __builtin__ import file
 
 #configurations or params
 file = 'in.xml'
@@ -48,42 +49,46 @@ def parse(file, orgs, result, name, url):
     actST = 0
     items = {}
     locs = {}
-    for event, elem in ET.iterparse(file, events=('start', 'end')):
-        if event == 'start':
-            if elem.tag == 'iati-activity':
-                actST = 1
-            if elem.tag == 'location':
-                for loc in elem:
-                    if loc.tag == 'name':
-                        locs[unicode(loc.text)] = unicode(loc.text)
-            if elem.tag in orgs:
-                item = {}
-                orgName = unicode(elem.text)
-                item['orgName'] = orgName  # orgName.encode('utf-8')
-#                 if 'ref' in elem.attrib:
-                item['orgCode'] = unicode(elem.attrib.get('ref', None))
-                if (item['orgCode'] and item['orgCode'] != u'None' ) or  \
-                        (item['orgName'] and item['orgName'].strip()):
-                    item['orgID'] = unicode(
-                        item['orgName'].strip().lower().replace(
-                            ' ', '__') + '#' + item['orgCode'])
-                    orgID = item['orgID']
-                    item['sourceName'] = unicode(name)
-                    item['sourceURL'] = unicode(url)
-                    if 'role' in elem.attrib:
-                        item['orgRole'] = unicode(elem.attrib['role'])
-                    item['location'] = []
-                    if orgID not in items:
-                        items[orgID] = [item]
-                    else:
-                        items[orgID].append(item)
-        if event == 'end':
-            if elem.tag == 'iati-activity':
-                for it in items:
-                    addOrgList(result, it, items[it], locs)
-                actST = 0
-                items = {}
-                locs = {}
+    try:
+        for event, elem in ET.iterparse(file, events=('start', 'end')):
+            if event == 'start':
+                if elem.tag == 'iati-activity':
+                    actST = 1
+                if elem.tag == 'location':
+                    for loc in elem:
+                        if loc.tag == 'name':
+                            locs[unicode(loc.text)] = unicode(loc.text)
+                if elem.tag in orgs:
+                    item = {}
+                    orgName = unicode(elem.text)
+                    item['orgName'] = orgName  # orgName.encode('utf-8')
+    #                 if 'ref' in elem.attrib:
+                    item['orgCode'] = unicode(elem.attrib.get('ref', None))
+                    if (item['orgCode'] and item['orgCode'] != u'None' ) or  \
+                            (item['orgName'] and item['orgName'].strip()):
+                        item['orgID'] = unicode(
+                            item['orgName'].strip().lower().replace(
+                                ' ', '__') + '#' + item['orgCode'])
+                        orgID = item['orgID']
+                        item['sourceName'] = unicode(name)
+                        item['sourceURL'] = unicode(url)
+                        if 'role' in elem.attrib:
+                            item['orgRole'] = unicode(elem.attrib['role'])
+                        item['location'] = []
+                        if orgID not in items:
+                            items[orgID] = [item]
+                        else:
+                            items[orgID].append(item)
+            if event == 'end':
+                if elem.tag == 'iati-activity':
+                    for it in items:
+                        addOrgList(result, it, items[it], locs)
+                    actST = 0
+                    items = {}
+                    locs = {}
+    except ET.ParseError, e:
+        print 'Could not parse as xml: ' + file
+        print str(e)
     return result
 
 
@@ -115,6 +120,9 @@ def run_parse(transformer, pusher):
             f_name = data[str(i)]['name']
             print 'START parsing ... ' + f_file + ' from URL: ' + f_url
             res = parse_file(f_file, f_name, f_url)
-            org_dict = transformer.transform(res)
-            pusher.push(org_dict)
-            print 'END parsing ... ' + f_file
+            if res:
+                org_dict = transformer.transform(res)
+                pusher.push(org_dict)
+                print 'END parsing ... ' + f_file
+            else:
+                print 'NOT parsing, wrong format: ' + f_file
