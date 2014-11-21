b2f94a39f38ffd95fd3584ee7e801c7e4cd3ad2b
Alexandru Artimon <aalecs@gmail.com>
2014-11-21 16:33:51 +0200

    adding graph

---
+++ solr/org_collection/conf/velocity/graph.js
@@ -0,0 +1,39 @@
+$(document).ready(
+  function (){
+    $('.json').each(function() {
+      var th = $(this);
+      var txtData = th.text();
+      txtData = txtData.slice(0, txtData.length - 4) + "]";
+      data = JSON.parse(txtData);
+      th.text("");
+      th.attr("style", "");
+
+      var chart = c3.generate({
+        bindto: this,
+        color: {
+          pattern: ['#007ce0']
+        },
+        data: {
+          json: data,
+          keys: {
+            x: 'code',
+            value: ['value']
+          },
+          x: 'x',
+          type: 'bar'
+        },
+        axis: {
+          x: {
+            type: 'category',
+            tick: {
+              rotate: 20
+            }
+          },
+          y: {
+            show: false
+          }
+        }
+      });
+    });
+
+  });
